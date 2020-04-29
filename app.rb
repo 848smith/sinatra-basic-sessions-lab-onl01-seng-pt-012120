5bf109a65445bb3eea254f8b7ccde55f23ac42a2
Andrew Smith <848smith@gmail.com>
2020-04-29 01:15:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do :sessions
+    enable :sessions
+    set :session_secret, "03062017"
+  end
+  
+  get '/' do
+    erb :index
+  end
 end
